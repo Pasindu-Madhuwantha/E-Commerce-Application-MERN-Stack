c622f6ebca3445dc707c453e8f2414c0695e76f2
Pasindu-Madhuwantha <mpasindu72@gmail.com>
2023-02-27 23:01:46 +0530

    created update and delete functionalities

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -1,6 +1,6 @@
 const Product = require('../models/product')
 
-//create new product => /api/v1/product/new
+//create new product => /api/v1/seller/product/new
 exports.newProduct = async(req,res,next) =>{
     const product = await Product.create(req.body);
 
@@ -41,6 +41,59 @@ exports.getSingleProduct = async(req, res, next) =>{
         success:true,
         product
     })
+}
+
+
+//Update Product => /api/v1/seller/prdouct/:id
+exports.updateProduct = async(req, res, next) =>{
+
+    let product = await Product.findById(req.params.id);
+
+    if(!product){
+        return res.status(404).json({
+            success:false,
+            message: 'Product not found'
+        })
+    }
+
+    product = await Product.findByIdAndUpdate(req.params.id,req.body,{
+        new: true,
+        runValidators:true,
+        useFindAndModify:false
+
+    });
+    res.status(200).json({
+        success:true,
+        product
+    })
+
+
+
+}
+
+//Delete product => /api/v1/admin/product/:id
+exports.deleteProduct = async(req,res,next) =>{
+  
+    const product = await Product.findById(req.params.id);
+
+    if(!product){
+        return res.status(404).json({
+            success:false,
+            message: 'Product not found'
+        })
+    }
+
+    await product.remove();
+
+    res.status(200).json({
+        success:true,
+        message:'Product is deleted.'
+    })
+
+
+
+
+
 }
 
 
