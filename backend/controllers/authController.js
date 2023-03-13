ac8d55ffa0dfdcf1424d10737cc9208d9dd3ebb3
Pasindu-Madhuwantha <mpasindu72@gmail.com>
2023-03-13 20:38:41 +0530

    frontend header and footer added

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -1,4 +1,4 @@
-const User = require('../models/user');
+const User = require('../models/seller');
 const ErrorHandler = require('../utils/errorHandler');
 const catchAsyncErrors = require('../middlewares/catchAsyncErrors');
 const sendToken = require('../utils/jwtToken');
